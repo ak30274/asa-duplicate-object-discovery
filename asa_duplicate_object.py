3dcde176e6c8371608a022db7ca9b30051fc841b
aklyuchk <aklyuchk@cisco.com>
2017-07-23 22:26:39 -0700

    corrected pip8 related issues

--- asa_duplicate_object.py
+++ asa_duplicate_object.py
@@ -12,208 +12,233 @@ import sys
 import datetime
 from ciscoconfparse import CiscoConfParse
 
-# Function to create dictionary of all objects and object-groups found in the configuration
-def create_dict_object(input_raw,input_parse):
-  objects = {}
-  object_groups = {}
-  for line in input_raw:
-    # Identify network objects by matching the config line
-    if 'object network' in line or 'object protocol' in line or 'object service' in line:
-      # Find all child configurations components for this specific line
-      object = input_parse.find_children_w_parents(line,'.*')
-      # Isolate the name by popping it off the end of string
-      name = (line.split()).pop(2)
-      # We also need the first word in order to later ignore it if is a description
-      first = (line.split()).pop(0)
-      # Verify we haven't come across this object already and then create add it to the object 
-      # dictionary with the child configuration provided as a list
-      if not name in objects and first != "description":
-        objects[name] = (object)
-    # Repeating the above process but identifying object-groups instead
+
+# Function to create dictionary of all objects and object-groups found in
+# the configuration
+def create_dict_object(input_raw, input_parse):
+    objects = {}
+    object_groups = {}
+    for line in input_raw:
+        # Identify network objects by matching the config line
+        if 'object network' in line or 'object protocol' in line or 'object service' in line:
+            # Find all child configurations components for this specific line
+            object = input_parse.find_children_w_parents(line, '.*')
+            # Isolate the name by popping it off the end of string
+            name = (line.split()).pop(2)
+            # We also need the first word in order to later ignore it if is a
+            # description
+            first = (line.split()).pop(0)
+            # Verify we haven't come across this object already and then create add it to the object
+            # dictionary with the child configuration provided as a list
+            if not name in objects and first != "description":
+                objects[name] = (object)
+                # Repeating the above process but identifying object-groups
+                # instead
     if 'object-group network' in line or 'object-group protocol' in line or 'object-group service' in line:
-      object_group = input_parse.find_children_w_parents(line,'.*')
-      name = (line.split()).pop(2)
-      first = (line.split()).pop(0)
-      if not name in object_groups and first != "description":
-        object_groups[name] = (object_group)
-  # Return two dictionaries - one for objects and one for object groups
-  return (objects,object_groups)
+        object_group = input_parse.find_children_w_parents(line, '.*')
+        name = (line.split()).pop(2)
+        first = (line.split()).pop(0)
+        if not name in object_groups and first != "description":
+            object_groups[name] = (object_group)
+            # Return two dictionaries - one for objects and one for object
+            # groups
+            return (objects, object_groups)
 
 # This function identifies duplicate objects by iterating through the dictionary and looking
 # for value matches
+
+
 def check_dup_object(object_dictionary):
-  duplicates = {}
-  already_found = []
-  #Yeah, I know the following is a hackers code...that's me.  Get the job done.  Fix it later.
-  for k,v in object_dictionary.items():
-    item = []
-    # We don't want multiple iterations of the same matches - once an object has been processed
-    # or found to be a duplicate, it is added to already_found and ignored in futer iterations
-    if not k in already_found:
-      for x,y in object_dictionary.items():
-        # Looking to verify that the value from our source object matches the value of another
-        # and both aren't empty
-        if v == y and v and y:
-          # Verify we haven't matched our own object name and then add the matched object name
-          # to the list
-          if k != x:
-            item.append(x)
-            if not x in already_found:
-              already_found.append(x)
-      already_found.append(k)
-    # If we've matched - apply that match to the return dictionary
-    if item:
-      duplicates[k] = item
-  # This should now be a dictionary with keys being an object and values being all of the matching
-  # objects that were found.
-  return (duplicates)
+    duplicates = {}
+    already_found = []
+    # Yeah, I know the following is a hackers code...that's me.  Get the job
+    # done.  Fix it later.
+    for k, v in object_dictionary.items():
+        item = []
+        # We don't want multiple iterations of the same matches - once an object has been processed
+        # or found to be a duplicate, it is added to already_found and ignored
+        # in futer iterations
+        if not k in already_found:
+            for x, y in object_dictionary.items():
+                # Looking to verify that the value from our source object matches the value of another
+                # and both aren't empty
+                if v == y and v and y:
+                    # Verify we haven't matched our own object name and then add the matched object name
+                    # to the list
+                    if k != x:
+                        item.append(x)
+                        if not x in already_found:
+                            already_found.append(x)
+            already_found.append(k)
+        # If we've matched - apply that match to the return dictionary
+        if item:
+            duplicates[k] = item
+    # This should now be a dictionary with keys being an object and values being all of the matching
+    # objects that were found.
+    return (duplicates)
 
 # This function identifies duplicate object groups by iterating through the dictionary and looking
 # for value matches
+
+
 def check_dup_object_group(object_group_dictionary):
-  duplicates = {}
-  already_found = []
-  #Yeah, I know the following is a hackers code...that's me.  Get the job done.  Fix it later.
-  for k,v in object_group_dictionary.items():
-    item = []
-    # We don't want multiple iterations of the same matches - once an object group has been processed
-    # or found to be a duplicate, it is added to already_found and ignored in futer iterations
-    if not k in already_found:
-      for x,y in object_group_dictionary.items():
-        # Looking to verify that the value from our source object group matches the value of another
-        # and both aren't empty
-        if v == y and v and y:
-          # Verify we haven't matched our own object group name and then add the matched object name
-          # to the list
-          if k != x:
-            item.append(x)
-            if not x in already_found:
-              already_found.append(x)
-      already_found.append(k)
-    # If we've matched - apply that match to the return dictionary
-    if item:
-      duplicates[k] = item
-  # This should now be a dictionary with keys being an object group and values being all of the matching
-  # object groups that were found.
-  return (duplicates)
+    duplicates = {}
+    already_found = []
+    # Yeah, I know the following is a hackers code...that's me.  Get the job
+    # done.  Fix it later.
+    for k, v in object_group_dictionary.items():
+        item = []
+        # We don't want multiple iterations of the same matches - once an object group has been processed
+        # or found to be a duplicate, it is added to already_found and ignored
+        # in futer iterations
+        if not k in already_found:
+            for x, y in object_group_dictionary.items():
+                # Looking to verify that the value from our source object group matches the value of another
+                # and both aren't empty
+                if v == y and v and y:
+                    # Verify we haven't matched our own object group name and then add the matched object name
+                    # to the list
+                    if k != x:
+                        item.append(x)
+                        if not x in already_found:
+                            already_found.append(x)
+            already_found.append(k)
+        # If we've matched - apply that match to the return dictionary
+        if item:
+            duplicates[k] = item
+    # This should now be a dictionary with keys being an object group and values being all of the matching
+    # object groups that were found.
+    return (duplicates)
 
 
 # This function is used to write detailed output to a file for future review
-def write_to_file(objects,object_groups,input_parse,output_file):
-  today = datetime.date.today()
-  # Open the file for writing.
-  f = open(output_file,'w')
-  f.write("Output For asa_duplicate_object\n")
-  f.write("Date: " + today.ctime() + "\n")
-  f.write("Input File:  " + sys.argv[1] + "\n\n\n")
+def write_to_file(objects, object_groups, input_parse, output_file):
+    today = datetime.date.today()
+    # Open the file for writing.
+    f = open(output_file, 'w')
+    f.write("Output For asa_duplicate_object\n")
+    f.write("Date: " + today.ctime() + "\n")
+    f.write("Input File:  " + sys.argv[1] + "\n\n\n")
 
-  # Object Output
+    # Object Output
 
-  f.write("------------------------------\n     Object Duplicates\n------------------------------\n\n\n")
-  i = 1
-  for k,v in objects.items():
-    f.write("-----------------\nDuplicate Item " + str(i) + "\n-----------------\n\n")
-    #Write primary object out
-    f.write("First Object Found:\n\n")
-    primary = input_parse.find_all_children('object (network|protocol|service) ' + k)
-    for lines in primary:
-      f.write("\t" + lines)
-    f.write("\n\n")
-    #Write ancillary objects out
-    f.write("Identical Object(s):\n\n")
-    for items in objects[k]:
-      secondary = input_parse.find_all_children('object (network|protocol|service) ' + items)
-      for sec in secondary:
-        f.write("\t" + sec)
-      f.write("\n")
-    f.write("\n\n")
-    #Write dependent configurations of primary
-    f.write("Configuration Dependent On Primary Object:\n\n")
-    depend_primary = input_parse.find_blocks(k)
-    for dep_pri in depend_primary:
-      f.write("\t" + dep_pri)
-    f.write("\n\n")
-    #Write dependent configurations of primary
-    f.write("Configuration Dependent On Secondary Object(s):\n\n")
-    for more_items in objects[k]:
-      depend_secondary = input_parse.find_blocks(more_items)
-      for dep_sec in depend_secondary:
-        f.write("\t" + dep_sec)
+    f.write("------------------------------\n     Object Duplicates\n------------------------------\n\n\n")
+    i = 1
+    for k, v in objects.items():
+        f.write("-----------------\nDuplicate Item " +
+                str(i) + "\n-----------------\n\n")
+        # Write primary object out
+        f.write("First Object Found:\n\n")
+        primary = input_parse.find_all_children(
+            'object (network|protocol|service) ' + k)
+        for lines in primary:
+            f.write("\t" + lines)
+        f.write("\n\n")
+        # Write ancillary objects out
+        f.write("Identical Object(s):\n\n")
+        for items in objects[k]:
+            secondary = input_parse.find_all_children(
+                'object (network|protocol|service) ' + items)
+            for sec in secondary:
+                f.write("\t" + sec)
+            f.write("\n")
+        f.write("\n\n")
+        # Write dependent configurations of primary
+        f.write("Configuration Dependent On Primary Object:\n\n")
+        depend_primary = input_parse.find_blocks(k)
+        for dep_pri in depend_primary:
+            f.write("\t" + dep_pri)
+        f.write("\n\n")
+        # Write dependent configurations of primary
+        f.write("Configuration Dependent On Secondary Object(s):\n\n")
+        for more_items in objects[k]:
+            depend_secondary = input_parse.find_blocks(more_items)
+            for dep_sec in depend_secondary:
+                f.write("\t" + dep_sec)
+        f.write("\n\n\n\n\n\n")
+        i += 1
     f.write("\n\n\n\n\n\n")
-    i += 1
-  f.write("\n\n\n\n\n\n")
-  
-  # Object Group Output
 
-  f.write("------------------------------\n     Object Group Duplicates\n------------------------------\n\n\n")
-  i = 1
-  for k,v in object_groups.items():
-    f.write("-----------------\nDuplicate Item " + str(i) + "\n-----------------\n\n")
-    #Write primary object out
-    f.write("First Object Group Found:\n\n")
-    primary = input_parse.find_all_children('object-group (network|protocol|service) ' + k)
-    for lines in primary:
-      f.write("\t" + lines)
-    f.write("\n\n")
-    #Write ancillary objects out
-    f.write("Identical Object Group(s):\n\n")
-    for items in object_groups[k]:
-      secondary = input_parse.find_all_children('object-group (network|protocol|service) ' + items)
-      for sec in secondary:
-        f.write("\t" + sec)
-      f.write("\n")
-    f.write("\n\n")
-    #Write dependent configurations of primary
-    f.write("Configuration Dependent On Primary Object Group:\n\n")
-    depend_primary = input_parse.find_blocks(k)
-    for dep_pri in depend_primary:
-      f.write("\t" + dep_pri)
-    f.write("\n\n")
-    #Write dependent configurations of primary
-    f.write("Configuration Dependent On Secondary Object Group(s):\n\n")
-    for more_items in object_groups[k]:
-      depend_secondary = input_parse.find_blocks(more_items)
-      for dep_sec in depend_secondary:
-        f.write("\t" + dep_sec)
+    # Object Group Output
+
+    f.write("------------------------------\n     Object Group Duplicates\n------------------------------\n\n\n")
+    i = 1
+    for k, v in object_groups.items():
+        f.write("-----------------\nDuplicate Item " +
+                str(i) + "\n-----------------\n\n")
+        # Write primary object out
+        f.write("First Object Group Found:\n\n")
+        primary = input_parse.find_all_children(
+            'object-group (network|protocol|service) ' + k)
+        for lines in primary:
+            f.write("\t" + lines)
+        f.write("\n\n")
+        # Write ancillary objects out
+        f.write("Identical Object Group(s):\n\n")
+        for items in object_groups[k]:
+            secondary = input_parse.find_all_children(
+                'object-group (network|protocol|service) ' + items)
+            for sec in secondary:
+                f.write("\t" + sec)
+            f.write("\n")
+        f.write("\n\n")
+        # Write dependent configurations of primary
+        f.write("Configuration Dependent On Primary Object Group:\n\n")
+        depend_primary = input_parse.find_blocks(k)
+        for dep_pri in depend_primary:
+            f.write("\t" + dep_pri)
+        f.write("\n\n")
+        # Write dependent configurations of primary
+        f.write("Configuration Dependent On Secondary Object Group(s):\n\n")
+        for more_items in object_groups[k]:
+            depend_secondary = input_parse.find_blocks(more_items)
+            for dep_sec in depend_secondary:
+                f.write("\t" + dep_sec)
+        f.write("\n\n\n\n\n\n")
+        i += 1
     f.write("\n\n\n\n\n\n")
-    i += 1
-  f.write("\n\n\n\n\n\n")
-  f.close()
+    f.close()
 
 
 # This function doesn't do anything, that's why I named it main.
 def main():
-  # Verify we have the right number of arguments.
-  if len(sys.argv) > 1 and len(sys.argv) < 4:
-    # Check to see if a file-name argument was given and if so assign it to the output variable.  Otherwise assign a generic output.txt
-    print "\n\nRunning script to identity duplicte objects and object-groups in the provided ASA configuration...\n\n\n\n"
-    if len(sys.argv) >= 3:
-      output = sys.argv[2]
-    else:
-      output = "output.txt"
-    # This is our source config file, it's important.
-    x = open(sys.argv[1])
-    # Read the source file into a varaible for future use
-    config_raw = x.readlines()
-    # This is a parsed version of the config using CiscoConfParse, also very important
-    config_parse = CiscoConfParse(config_raw) 
-    x.close()
+    # Verify we have the right number of arguments.
+    if len(sys.argv) > 1 and len(sys.argv) < 4:
+        # Check to see if a file-name argument was given and if so assign it to
+        # the output variable.  Otherwise assign a generic output.txt
+        print "\n\nRunning script to identity duplicte objects and object-groups in the provided ASA configuration...\n\n\n\n"
+        if len(sys.argv) >= 3:
+            output = sys.argv[2]
+        else:
+            output = "output.txt"
+        # This is our source config file, it's important.
+        x = open(sys.argv[1])
+        # Read the source file into a varaible for future use
+        config_raw = x.readlines()
+        # This is a parsed version of the config using CiscoConfParse, also
+        # very important
+        config_parse = CiscoConfParse(config_raw)
+        x.close()
 
-    # Real work starts - take the config and sent it off to be broken down into object and object group dictionaries
-    object_dict, object_group_dict = create_dict_object(config_raw,config_parse)
-    # Use the above dictionaries to see if we have duplicate objects or object groups
-    object_dups = check_dup_object(object_dict)
-    object_group_dups = check_dup_object_group(object_group_dict)
-    # Share what we've learned with the output file
-    write_to_file(object_dups,object_group_dups,config_parse,output)
-    print "Script execution is complete and results have been written to " + output + "\n\n"
-  else:
-    print "\n\n**** ERROR ****\nIncorrect Number Of Arguments\n***************\n\nUsage:  python asa_duplicate_object.py 'configuration_file_name' 'output_file_name'\n\noutput_file_name is an optional argument.\n\n"
+        # Real work starts - take the config and sent it off to be broken down
+        # into object and object group dictionaries
+        object_dict, object_group_dict = create_dict_object(
+            config_raw, config_parse)
+        # Use the above dictionaries to see if we have duplicate objects or
+        # object groups
+        object_dups = check_dup_object(object_dict)
+        object_group_dups = check_dup_object_group(object_group_dict)
+        # Share what we've learned with the output file
+        write_to_file(object_dups, object_group_dups, config_parse, output)
+        print "Script execution is complete and results have been written to " + output + "\n\n"
+    else:
+        print "\n\n**** ERROR ****\nIncorrect Number Of Arguments\n***************\n\nUsage:  python asa_duplicate_object.py 'configuration_file_name' 'output_file_name'\n\noutput_file_name is an optional argument.\n\n"
 
 
 if __name__ == '__main__':
-  main()
+    main()
 
 
 # If you've made it this far I'm impressed.  I know I'm a hack of programmer but it does what I need it to.
-# You should go grab a beer or something to lower your anxiety from reading this code.
+# You should go grab a beer or something to lower your anxiety from
+# reading this code.
